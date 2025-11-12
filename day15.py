d0cf7db5d756e951b72dfbf067d3b5a2b2b4db5e
sujal <priyanshupantnagar@gmail.com>
2025-11-12 22:37:10 +0530

    match case

--- day15.py
+++ day15.py
@@ -2,7 +2,7 @@
 x = int(input("Enter the value of x:"))
 match x:
     case 0:
-        print("x is zero")
+        print("x is zero ")
     case 4:
         print("case is 4")
     case _ if x!= 90:
