ffed572c183f0370516aee97d5409569688a7f7b
sujal <priyanshupantnagar@gmail.com>
2025-11-13 21:57:56 +0530

    first python program

--- day3.py
+++ day3.py
@@ -3,7 +3,7 @@ print("Hello World", 7)
 print(5)
 print("Bye")
 print(17*12)
-print("Hiiii")
+print("Hiiiii")
 
 # print a poem of my choice 
 print("twinkle twinkle little star\nhow i wonder what you are,\nup above the world so high,\nlike a diamond in the sky")
