efa718d753bd649aaebad2ab3efcede2aca46e1c
sujal <priyanshupantnagar@gmail.com>
2025-11-14 22:03:16 +0530

    basic calc

--- day7.py
+++ day7.py
@@ -4,5 +4,5 @@ b = 10
 
 print("sum of a and b is",(a+b))
 print("difference of a and b is",(a-b))
-print("multipication of a and b is",(a*b))
+print("Multipication of a and b is",(a*b))
 print("divison of a and b is",(a/b))
