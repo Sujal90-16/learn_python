b11a5fdb1a9f686d7c3dcb4ae041fefb2819d505
sujal <priyanshupantnagar@gmail.com>
2025-11-15 21:07:59 +0530

    greetings

--- day14.py
+++ day14.py
@@ -7,7 +7,7 @@ print("Current Hour:", hour)
 if hour >= 0 and hour < 12:
     print("Good morning sir!!")
 elif hour >= 12 and hour < 16:
-    print("Good afternoon sir!!!")
+    print("Good afternoon Sir!!!")
 elif hour >= 16 and hour <20:
     print("Good evening sir!!!")
 else:
