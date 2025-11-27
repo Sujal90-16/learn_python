f0645a7f4f8804e07260ca551396435b4ab20fef
sujal <priyanshupantnagar@gmail.com>
2025-11-27 18:49:17 +0530

    for loop with else

--- day33.py
+++ day33.py
@@ -1,7 +1,11 @@
 # for loop with else 
-for i in range(6):
-    print(i)
-    if i == 4:
-        break
-else:
-    print("Sorry no i")
+# for i in range(6):
+#     print(i)
+#     if i == 4:
+#         break
+# else:
+#     print("Sorry no i")
+
+i = 0
+while i<7:
+    
